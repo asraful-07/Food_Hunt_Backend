372af09e4822d6d68b6f4037684255907000495a
asraful-07 <asraful0508@gmail.com>
2026-03-20 16:22:02 +0600

    fix tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["esm"],
-  platform: "node",
-  target: "node20",
-  outDir: "api",
-  external: ["pg-native"],
-  shims: true,
-  banner: {
-    js: `import { createRequire } from 'module';
-const require = createRequire(import.meta.url);`,
-  },
+  entry: ["src/server.ts"],
+  format: ["cjs"],
+  outDir: "dist",
+  clean: true,
 });
